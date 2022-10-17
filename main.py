e57c1eeb7e0253fc5c1090e764ac155db22bef34
gingerwitch64 <redpendrew@gmail.com>
2022-10-17 12:08:25 -0400

    Started writing code, forgot to do separate commits

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+def scour(text):
+    words = text.split()
+    for word in words:
+        if word.find() != -1:
+        # TODO: Find word from list
+
+def main():
+    window = tk.Tk()
+    input = tk.Text()
+
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
