1dbf1a81962262c9b2416d5fe2f9976d8a7307f6
gingerwitch64 <redpendrew@gmail.com>
2022-10-18 17:39:44 -0400

    main.py: Program is in functional state.

--- main.py
+++ main.py
@@ -3,21 +3,56 @@ import tkinter as tk
 kwfile = open("keywords.txt", "r")
 keywords = kwfile.read().split(",")
 kwfile.close()
+punct = [".","?","!","\n"]
 
-def scour(text):
+def scour():
+    text = input.get("1.0",tk.END)
     out = []
+    sentence = 1
+    sentword = 1
     words = text.split()
     for word in words:
-        if any(x in word for x in keywords) != -1:
-            out.append(word)
-        
+        if any(x in word for x in punct):
+            sentword = 0
+        if any(x in word.lower() for x in keywords):
+            out.append(f"Keyword found in '{word}' as word number {sentword} in sentence {sentence}.\n")
+        if any(x in word for x in punct):
+            sentence += 1
+        sentword += 1
+    output.delete("1.0", tk.END)
 
-def main():
-    window = tk.Tk()
-    input = tk.Text()
+    if out == []:
+        output.insert("1.0", "No keywords found.")
+    else:
+        for item in out:
+            output.insert(f"{out.index(item)+1}.0", item)
+
+### OBJECTS ###
+window = tk.Tk()
+#intxtfrm = tk.Frame(master=window)
+input = tk.Text(master=window)
+#inscr = tk.Scrollbar(master=input, command=input.yview)
+mnufrm = tk.Frame(master=window)
+output = tk.Text(master=mnufrm, height=1)
+btn = tk.Button(master=mnufrm, text="Go", command=scour)
+outscr = tk.Scrollbar(master=mnufrm, command=output.yview)
 
 
-    window.mainloop()
+### PACKING ###
+#intxtfrm.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
-if __name__ == "__main__":
-    main()
+input.pack(fill=tk.BOTH, expand=True)
+#inscr.pack(fill=tk.Y, side=tk.RIGHT, expand=True)
+#input['yscrollcommand'] = inscr.set
+
+mnufrm.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
+
+btn.pack(side=tk.LEFT, expand=False)
+
+output.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+outscr.pack(fill=tk.Y, side=tk.RIGHT, expand=True)
+output.insert("1.0", "Output will appear here.")
+output['yscrollcommand'] = outscr.set
+
+### WINDOW LOOP ###
+window.mainloop()
