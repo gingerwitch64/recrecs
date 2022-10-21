d234ed3ad122cdcb0f9259b335be45127e6fcc26
gingerwitch64 <redpendrew@gmail.com>
2022-10-21 09:54:58 -0400

    packaging: Start making packaging scripts

---
+++ packaging/package.bat
@@ -0,0 +1,4 @@
+echo "Make sure that both python, pip and pip's local script install location are added to PATH."
+echo "pip's local script location is usually C:\Users\YOURUSERNAME\AppData\Roaming\Python\PythonVERSION\Scripts"
+Rem pip install pyinstaller
+Rem pyinstaller --onefile
---
+++ packaging/package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir recrecs
+cp ../main.py ./recrecs/recrecs.pyw
+cp ../keywords.txt ./recrecs/
+chmod +x ./recrecs/recrecs.pyw
