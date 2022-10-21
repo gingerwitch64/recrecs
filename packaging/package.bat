9bacc2f3681b83824cf2508842051508306a0ff8
Nola G <redpendrew@gmail.com>
2022-10-21 17:25:18 -0400

    packaging: Finished build script

--- packaging/package.bat
+++ packaging/package.bat
@@ -1,4 +1,19 @@
 echo "Make sure that both python, pip and pip's local script install location are added to PATH."
 echo "pip's local script location is usually C:\Users\YOURUSERNAME\AppData\Roaming\Python\PythonVERSION\Scripts"
-Rem pip install pyinstaller
-Rem pyinstaller --onefile
+pip install pyinstaller || goto :error
+mkdir build || goto :error
+mkdir build\subbuild\ || goto :error
+copy ..\main.py .\build\subbuild\recrecs.pyw || goto :error
+cd build\subbuild\ || goto :error
+pyinstaller --onefile recrecs.pyw || goto :error
+copy .\dist\recrecs.exe .. || goto :error
+cd ..\.. || goto :error
+goto :success
+
+Rem Credit to https://stackoverflow.com/a/8965092
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
+
+:success
+echo "Successfully built. recrecs.exe is in build folder."
